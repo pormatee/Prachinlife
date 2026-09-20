751b5c04259988e6252addec9a7f0db68352fe35
pormatee <affi4716@gmail.com>
2026-09-20 09:09:58 +0700

    Add LocalLife compatibility boundary

---
+++ place_platform_v2/compat/__init__.py
@@ -0,0 +1,11 @@
+"""LocalLife compatibility boundary."""
+
+from .core_place_verification_v1 import (
+    COORDINATE_REPORT_NAMES,
+    evaluate_compatibility,
+)
+
+__all__ = [
+    "COORDINATE_REPORT_NAMES",
+    "evaluate_compatibility",
+]
